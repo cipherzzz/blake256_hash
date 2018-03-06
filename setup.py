63247a100136161835033fe1d8e3cce81a429482
cipherzzz <dev@cipherz.com>
2018-03-06 18:17:49 -0500

    package?

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="A Python module supporting blake256 hashing",
     license="MIT",
     author="cipherZ",
-    packages=["blake256_hash"],
+    packages=find_packages(),
     url="https://github.com/cipherzzz/blake256_hash",
     classifiers=[
         "Programming Language :: Python",
